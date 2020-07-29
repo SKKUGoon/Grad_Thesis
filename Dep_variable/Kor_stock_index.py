d986bf5616a8dd97e564599988fb02b1b5ac7890
SKKUGoon <baesangil0906@gmail.com>
2020-07-30 05:50:30 +0900

    rename lag 3 data

--- Dep_variable/Kor_stock_index.py
+++ Dep_variable/Kor_stock_index.py
@@ -21,10 +21,11 @@ WD = WD.log_return_class(0)
 WD.to_csv(r"D:\Data\Grad\Kor_log_ret_class_lag1.csv")
 
 # Lag : 3
+lag = 3
 WorkData = preprocessing(Data_kor)
 WorkData = WorkData.log_return(3, 0) # Even though the front most value is np.nan / Data will be cutted from 2001. So no worries
 WD = preprocessing(WorkData)
 WD = WD.log_return_class(0)
-WD = WD.rename(columns={'kor class': 'kor class_lag3'})
+WD = WD.rename(columns={'kor class': 'kor class_lag'+str(lag)})
 WD.to_csv(r"D:\Data\Grad\Kor_log_ret_class_lag3.csv")
 print(WD)
