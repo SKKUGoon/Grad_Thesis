dc8c455209a32e0736f3fb040bfd202b2cdc4d49
SKKUGoon <baesangil0906@gmail.com>
2020-08-06 05:27:00 +0900

    give new index(continuous date set) to the dataset

--- gathering/kor_additional.py
+++ gathering/kor_additional.py
@@ -1,5 +1,17 @@
 import investpy
 import pandas as pd
+import datetime
+
+start = datetime.datetime.strptime("02-01-2000", "%d-%m-%Y")
+end = datetime.datetime.strptime("06-06-2020", "%d-%m-%Y")
+date_gen = [start + datetime.timedelta(days=x) for x in range(0, (end - start).days)]
+date_dat = list()
+for date in date_gen:
+    date_dat.append(date.strftime("%Y-%m-%d"))
+base = pd.DataFrame(date_dat, columns=['Date'])
+base = base.set_index('Date')
+base.index = pd.to_datetime(base.index)
+
 # Insufficient Data
 KOSPIVIX = investpy.get_index_historical_data(index='KOSPI Volatility',
                                               country='south korea',
@@ -21,4 +33,5 @@ Kospi_volume = list(Kor['Volume'])
 K_volume = pd.DataFrame(list(Kor['Volume']), columns=['K_trade_volume'])
 K_volume.index = Kor.index
 K_volume.index = pd.to_datetime(K_volume.index)
+K_volume = pd.concat([base, K_volume], axis=1)
 K_volume.to_csv(r'D:\Data\Grad\Kospi_volume.csv')
