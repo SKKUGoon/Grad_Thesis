798493cfb5a349745750556f818d8a23e3783677
SKKUGoon <baesangil0906@gmail.com>
2020-08-11 20:18:01 +0900

    simplify

--- mlearning/scoringMethods.py
+++ mlearning/scoringMethods.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from typing import List
 
 def True_Positive(truth, pred):
     """the number of positive cases that are correctly identified as positive
@@ -48,168 +48,150 @@ def False_Negative(truth, pred):
     return FN
 
 class scoring_model:
-    def __init__(self, true_data, test_data):
+    def __init__(self, true_data: List, test_data: List):
         """Data is pandas core Series
         positive data has a value of 1, and negative data has a value of -1"""
         self.truth = true_data
         self.pred = test_data
+
         # Check if the length are same
         if len(self.truth) != len(self.pred):
             raise ValueError("Length of true data and test data doesn't match")
         else:
             pass
 
-    def sensitivity(self):
+        # Metrics needed
         self.TP = True_Positive(self.truth, self.pred)
         self.TN = True_Negative(self.truth, self.pred)
         self.FP = False_Positive(self.truth, self.pred)
         self.FN = False_Negative(self.truth, self.pred)
+
+    def accuracy(self) -> float:
+        accy = (self.TP + self.TN) / (self.TP + self.TN + self.FP + self.FN)
+        return accy
+
+
+    def sensitivity(self) -> float:
         if (self.TP + self.FN) == 0:
-            self.sens = np.nan
+            sens = np.nan
         else:
-            self.sens = self.TP / (self.TP + self.FN)
-        return self.sens
+            sens = self.TP / (self.TP + self.FN)
+        return sens
 
-    def specificity(self):
-        self.TP = True_Positive(self.truth, self.pred)
-        self.TN = True_Negative(self.truth, self.pred)
-        self.FP = False_Positive(self.truth, self.pred)
-        self.FN = False_Negative(self.truth, self.pred)
-        self.spec = self.TN / (self.TN + self.FP)
+
+    def specificity(self) -> float:
+        spec = self.TN / (self.TN + self.FP)
         if (self.TN + self.FP) == 0:
-            self.spec = np.nan
+            spec = np.nan
         else:
-            self.spec = self.TN / (self.TN + self.FP)
-        return self.spec
+            spec = self.TN / (self.TN + self.FP)
+        return spec
 
-    def gmean(self):
+
+    def gmean(self) -> float:
         """geometric mean.
         indicates balance between classification performance.
         poor performance in prediction of the positive cases will lead to low G-mean
         even if the negative cases are correctly classified by the evaluated algorithm"""
-        self.TP = True_Positive(self.truth, self.pred)
-        self.TN = True_Negative(self.truth, self.pred)
-        self.FP = False_Positive(self.truth, self.pred)
-        self.FN = False_Negative(self.truth, self.pred)
-
         if (self.TN + self.FP) == 0:
-            self.spec = np.nan
+            spec = np.nan
         else:
-            self.spec = self.TN / (self.TN + self.FP)
+            spec = self.TN / (self.TN + self.FP)
 
         if (self.TP + self.FN) == 0:
-            self.sens = np.nan
+            sens = np.nan
         else:
-            self.sens = self.TP / (self.TP + self.FN)
-        return np.sqrt(self.sens * self.spec)
+            sens = self.TP / (self.TP + self.FN)
+        return np.sqrt(sens * spec)
 
-    def LP(self):
+
+    def LP(self) -> float:
         """likelihood ratio represents the ratio between
         the probability of predicting an example as positive when it's actually positive
         &
         the probability of predicting an example as positive when it's not actually positive"""
-        self.TP = True_Positive(self.truth, self.pred)
-        self.TN = True_Negative(self.truth, self.pred)
-        self.FP = False_Positive(self.truth, self.pred)
-        self.FN = False_Negative(self.truth, self.pred)
         if (self.TN + self.FP) == 0:
-            self.spec = np.nan
+            spec = np.nan
         else:
-            self.spec = self.TN / (self.TN + self.FP)
+            spec = self.TN / (self.TN + self.FP)
 
         if (self.TP + self.FN) == 0:
-            self.sens = np.nan
+            sens = np.nan
         else:
-            self.sens = self.TP / (self.TP + self.FN)
-        return self.sens / (1 - self.spec)
+            sens = self.TP / (self.TP + self.FN)
+        return sens / (1 - spec)
 
-    def LR(self):
+
+    def LR(self) -> float:
         """negative likelihood ratio"""
-        self.TP = True_Positive(self.truth, self.pred)
-        self.TN = True_Negative(self.truth, self.pred)
-        self.FP = False_Positive(self.truth, self.pred)
-        self.FN = False_Negative(self.truth, self.pred)
         if (self.TN + self.FP) == 0:
-            self.spec = np.nan
+            spec = np.nan
         else:
-            self.spec = self.TN / (self.TN + self.FP)
+            spec = self.TN / (self.TN + self.FP)
 
         if (self.TP + self.FN) == 0:
-            self.sens = np.nan
+            sens = np.nan
         else:
-            self.sens = self.TP / (self.TP + self.FN)
-        return (1 - self.sens) / self.spec
+            sens = self.TP / (self.TP + self.FN)
+        return (1 - sens) / spec
 
-    def DP(self):
+
+    def DP(self) -> float:
         """Discriminant power. Summarizes sensitivity and specificity
         DP value higher than 3 indicate that the algorithm distinguishes well between positive & negative"""
-        self.TP = True_Positive(self.truth, self.pred)
-        self.TN = True_Negative(self.truth, self.pred)
-        self.FP = False_Positive(self.truth, self.pred)
-        self.FN = False_Negative(self.truth, self.pred)
         if (self.TN + self.FP) == 0:
-            self.spec = np.nan
+            spec = np.nan
         else:
-            self.spec = self.TN / (self.TN + self.FP)
+            spec = self.TN / (self.TN + self.FP)
 
         if (self.TP + self.FN) == 0:
-            self.sens = np.nan
+            sens = np.nan
         else:
-            self.sens = self.TP / (self.TP + self.FN)
-        return (np.sqrt(3)/np.pi)*(np.log(self.sens / (1 - self.sens))
-                                  + np.log(self.spec / (1 - self.spec)))
+            sens = self.TP / (self.TP + self.FN)
+        return (np.sqrt(3)/np.pi) * (np.log(sens / (1 - sens)) + np.log(spec / (1 - spec)))
 
-    def Youden(self):
+
+    def Youden(self) -> float:
         """Youden's gamma index
         linear transformation of the mean sensitivity and specifity
         Higher value of youden's gamma indicates better ability of the algorithm to avoid misclassification"""
-        self.TP = True_Positive(self.truth, self.pred)
-        self.TN = True_Negative(self.truth, self.pred)
-        self.FP = False_Positive(self.truth, self.pred)
-        self.FN = False_Negative(self.truth, self.pred)
         if (self.TN + self.FP) == 0:
-            self.spec = np.nan
+            spec = np.nan
         else:
-            self.spec = self.TN / (self.TN + self.FP)
+            spec = self.TN / (self.TN + self.FP)
 
         if (self.TP + self.FN) == 0:
-            self.sens = np.nan
+            sens = np.nan
         else:
-            self.sens = self.TP / (self.TP + self.FN)
-        return self.sens - (1 - self.spec)
+            sens = self.TP / (self.TP + self.FN)
+        return sens - (1 - spec)
 
-    def BA(self):
+
+    def BA(self) -> float:
         """Balanced accuracy metric is the average of sensitivity and specificity"""
-        self.TP = True_Positive(self.truth, self.pred)
-        self.TN = True_Negative(self.truth, self.pred)
-        self.FP = False_Positive(self.truth, self.pred)
-        self.FN = False_Negative(self.truth, self.pred)
         if (self.TN + self.FP) == 0:
-            self.spec = np.nan
+            spec = np.nan
         else:
-            self.spec = self.TN / (self.TN + self.FP)
+            spec = self.TN / (self.TN + self.FP)
 
         if (self.TP + self.FN) == 0:
-            self.sens = np.nan
+            sens = np.nan
         else:
-            self.sens = self.TP / (self.TP + self.FN)
-        return (1/2)*(self.sens + self.spec)
+            sens = self.TP / (self.TP + self.FN)
+        return (1/2) * (sens + spec)
 
-    def WBA(self):
+
+    def WBA(self) -> float:
         """Weighted balanced accuracy"""
-        self.TP = True_Positive(self.truth, self.pred)
-        self.TN = True_Negative(self.truth, self.pred)
-        self.FP = False_Positive(self.truth, self.pred)
-        self.FN = False_Negative(self.truth, self.pred)
         if (self.TN + self.FP) == 0:
-            self.spec = np.nan
+            spec = np.nan
         else:
-            self.spec = self.TN / (self.TN + self.FP)
+            spec = self.TN / (self.TN + self.FP)
 
         if (self.TP + self.FN) == 0:
-            self.sens = np.nan
+            sens = np.nan
         else:
-            self.sens = self.TP / (self.TP + self.FN)
+            sens = self.TP / (self.TP + self.FN)
             # ADD NEXT TIME
         return None
 
