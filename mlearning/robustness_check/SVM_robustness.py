e7c78f1b0eea9bc2221212924b1d4f9b4be127f8
SKKUGoon <baesangil0906@gmail.com>
2020-08-25 04:30:32 +0900

    refract scaler, graph title.

--- mlearning/robustness_check/SVM_robustness.py
+++ mlearning/robustness_check/SVM_robustness.py
@@ -32,6 +32,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y,
 scale = MinMaxScaler() # Scale the data.
 scale.fit(x_train)
 x_train_s = pd.DataFrame(scale.transform(x_train), columns = list(x_train.columns))
+scale.fit(x_test)
 x_test_s = pd.DataFrame(scale.transform(x_test), columns = list(x_test.columns))
 date = pd.DataFrame(x_train.index)
 date_t = pd.DataFrame(x_test.index)
@@ -84,12 +85,12 @@ print(boostadd)
 
 # Graph
 x_ = list(range(1,20))
-
+tlt = [1, 50, 100]
 for i in range(3):
     plt.plot(x_, gm[list(gm.keys())[i]], color='b', linestyle='dashed', label='gmean')
     plt.plot(x_, acc[list(acc.keys())[i]], color='b', label='accuracy')
     plt.xlabel('sigma')
-    plt.title(f'C={i+1}')
+    plt.title(f'C={tlt[i]}')
     plt.legend()
     plt.show()
 
