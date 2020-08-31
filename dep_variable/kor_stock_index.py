9b6c389828df0c7386994ab4bad51546dad341fe
SKKUGoon <baesangil0906@gmail.com>
2020-09-01 01:09:21 +0900

    delete lag: 3 reason: no use

--- dep_variable/kor_stock_index.py
+++ dep_variable/kor_stock_index.py
@@ -12,14 +12,9 @@ Data = pd.read_csv(r"D:\Data\Grad\total_stock_index_dataset.csv")
 Data = Data.set_index(Data.columns[0])
 Data.index = pd.to_datetime(Data.index)
 Data_kor = pd.DataFrame(Data['kor'])
-Data_kor = Data_kor.interpolate(method='time') # Working Data.
+Data_kor = Data_kor.fillna(method='ffill') # Working Data.
 
 # Lag : 1
 WD1 = Preprocessing(Data_kor)
 WD1 = WD1.log_return_class(1, 0)
 WD1.to_csv(r"D:\Data\Grad\Kor_log_ret_class_lag1.csv")
-
-# Lag : 3
-WD3 = Preprocessing(Data_kor)
-WD3 = WD3.log_return_class(3, 0)
-WD3.to_csv(r"D:\Data\Grad\Kor_log_ret_class_lag3.csv")
